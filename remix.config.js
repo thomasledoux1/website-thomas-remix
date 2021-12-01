a9aba9c8d5bb646ec9632fd71becbb2b40bae660
Thomas Ledoux <tledoux@reference.be>
2021-12-01 08:14:49 +0100

    Attempt to fix vercel deploy

--- remix.config.js
+++ remix.config.js
@@ -2,9 +2,8 @@
  * @type {import('@remix-run/dev/config').AppConfig}
  */
 module.exports = {
-  appDirectory: "app",
-  browserBuildDirectory: "public/build",
-  publicPath: "/build/",
-  serverBuildDirectory: "build",
-  devServerPort: 8002
+  appDirectory: 'app',
+  browserBuildDirectory: 'public/build',
+  publicPath: '/build/',
+  serverBuildDirectory: 'api/build',
 };
